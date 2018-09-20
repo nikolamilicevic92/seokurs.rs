4d97bfedd3e0525e1841be644cb0b743fe9d37ef
Nebojsa Milicevic <shocksnm@gmail.com>
2018-09-20 23:47:29 +0200

    ...

--- storage/views/includes.nav.php
+++ storage/views/includes.nav.php
@@ -6,19 +6,19 @@
           <span id="menuToggle"></span>
       </li>
       <li><a class="menu-link <?php echo htmlspecialchars(isActive('/')); ?>" href="/">POČETNA</a></li>
-      <li><a class="menu-link <?php echo htmlspecialchars(isActive('seo-kurs')); ?>" href="seo-kurs">SEO KURS</a></li>
-      <li><a class="menu-link <?php echo htmlspecialchars(isActive('seo-optimizacija')); ?>" href="seo-optimizacija">SEO OPTIMIZACIJA</a></li>
-      <li><a class="menu-link <?php echo htmlspecialchars(isActive('konsultacije')); ?>" href="konsultacije">KONSULTACIJE</a></li>
-      <li><a class="menu-link <?php echo htmlspecialchars(isActive('portfolio')); ?>" href="portfolio">PORTFOLIO</a></li>
-      <li><a class="menu-link <?php echo htmlspecialchars(isActive('nasi-partneri')); ?>" href="nasi-partneri">NASI PARTNERI</a></li>
-      <li><a class="menu-link <?php echo htmlspecialchars(isActive('o-nama')); ?>" href="o-nama">O NAMA</a></li>
+      <li><a class="menu-link <?php echo htmlspecialchars(isActive('seo-kurs')); ?>" href="seo-kurs">KURS</a></li>
+      <li><a class="menu-link <?php echo htmlspecialchars(isActive('seo-optimizacija')); ?>" href="seo-optimizacija">OPTIMIZACIJA</a></li>
+      <li><a class="menu-link <?php echo htmlspecialchars(isActive('konsultacije')); ?>" href="konsultacije">SAVETOVANJE</a></li>
+      <li><a class="menu-link <?php echo htmlspecialchars(isActive('portfolio')); ?>" href="portfolio">PROJEKTI</a></li>
+      <li><a class="menu-link <?php echo htmlspecialchars(isActive('nasi-partneri')); ?>" href="nasi-partneri">PARTNERI</a></li>
+      <li><a class="menu-link <?php echo htmlspecialchars(isActive('o-nama')); ?>" href="o-nama">MENTOR</a></li>
       <li><a class="menu-link <?php echo htmlspecialchars(isActive('kontakt')); ?>" href="kontakt">KONTAKT</a></li>
-      <li><a class="menu-link <?php echo htmlspecialchars(isActive('blog')); ?>" href="blog">NOVOSTI</a></li>
+      <li><a class="menu-link <?php echo htmlspecialchars(isActive('blog')); ?>" href="blog">BLOG</a></li>
       
       <?php if(user()->status('guest')): ?>
-        <li><a class="menu-link <?php echo htmlspecialchars(isActive(LOGIN_URI)); ?>" href="<?php echo htmlspecialchars(LOGIN_URI); ?>">ULOGUJ SE</a></li>   
+        <li><a class="menu-link <?php echo htmlspecialchars(isActive(LOGIN_URI)); ?>" href="<?php echo htmlspecialchars(LOGIN_URI); ?>">LOG IN</a></li>   
       <?php else: ?>     
-        <li><a class="menu-link" href="/logout">IZLOGUJ SE</a></li>
+        <li><a class="menu-link" href="/logout">LOG OUT</a></li>
       <?php endif; ?>         
     </ul>
   </nav>
--- views/includes/nav.php
+++ views/includes/nav.php
@@ -6,19 +6,19 @@
           <span id="menuToggle"></span>
       </li>
       <li><a class="menu-link {{isActive('/')}}" href="/">POČETNA</a></li>
-      <li><a class="menu-link {{isActive('seo-kurs')}}" href="seo-kurs">SEO KURS</a></li>
-      <li><a class="menu-link {{isActive('seo-optimizacija')}}" href="seo-optimizacija">SEO OPTIMIZACIJA</a></li>
-      <li><a class="menu-link {{isActive('konsultacije')}}" href="konsultacije">KONSULTACIJE</a></li>
-      <li><a class="menu-link {{isActive('portfolio')}}" href="portfolio">PORTFOLIO</a></li>
-      <li><a class="menu-link {{isActive('nasi-partneri')}}" href="nasi-partneri">NASI PARTNERI</a></li>
-      <li><a class="menu-link {{isActive('o-nama')}}" href="o-nama">O NAMA</a></li>
+      <li><a class="menu-link {{isActive('seo-kurs')}}" href="seo-kurs">KURS</a></li>
+      <li><a class="menu-link {{isActive('seo-optimizacija')}}" href="seo-optimizacija">OPTIMIZACIJA</a></li>
+      <li><a class="menu-link {{isActive('konsultacije')}}" href="konsultacije">SAVETOVANJE</a></li>
+      <li><a class="menu-link {{isActive('portfolio')}}" href="portfolio">PROJEKTI</a></li>
+      <li><a class="menu-link {{isActive('nasi-partneri')}}" href="nasi-partneri">PARTNERI</a></li>
+      <li><a class="menu-link {{isActive('o-nama')}}" href="o-nama">MENTOR</a></li>
       <li><a class="menu-link {{isActive('kontakt')}}" href="kontakt">KONTAKT</a></li>
-      <li><a class="menu-link {{isActive('blog')}}" href="blog">NOVOSTI</a></li>
+      <li><a class="menu-link {{isActive('blog')}}" href="blog">BLOG</a></li>
       
       @if(user()->status('guest'))
-        <li><a class="menu-link {{isActive(LOGIN_URI)}}" href="{{LOGIN_URI}}">ULOGUJ SE</a></li>   
+        <li><a class="menu-link {{isActive(LOGIN_URI)}}" href="{{LOGIN_URI}}">LOG IN</a></li>   
       @else     
-        <li><a class="menu-link" href="/logout">IZLOGUJ SE</a></li>
+        <li><a class="menu-link" href="/logout">LOG OUT</a></li>
       @endif         
     </ul>
   </nav>
