82f4e87baa5ab3f63faedb7ccaca97131ba50fdc
Nebojsa Milicevic <shocksnm@gmail.com>
2018-09-21 15:42:12 +0200

    ....

--- storage/views/pages.client.o-nama.php
+++ storage/views/pages.client.o-nama.php
@@ -17,66 +17,42 @@
         <h3 <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t1']->id); ?>"><?php echo htmlspecialchars($cf['t1']->value); ?></h3>
         <div <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t1_p']->id); ?>"><?php echo htmlspecialchars($cf['t1_p']->value); ?></div>
         <div class="flex-center">
-<<<<<<< HEAD
           <img height="380" src="assets/media/mentor1.png" alt="">
-=======
-          <img style="" src="assets/media/course-image.jpeg" alt="">
->>>>>>> 836e9c93633212d37e6b27057c2e8b33d3b316dd
         </div>
       </li>
       <li>
         <h3 <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t2']->id); ?>"><?php echo htmlspecialchars($cf['t2']->value); ?></h3>
         <div <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t2_p']->id); ?>"><?php echo htmlspecialchars($cf['t2_p']->value); ?></div>
         <div class="flex-center">
-<<<<<<< HEAD
           <img height="380px" src="assets/media/Mentor2.png" alt="">
-=======
-          <img height="600" src="assets/media/course-image.jpeg" alt="">
->>>>>>> 836e9c93633212d37e6b27057c2e8b33d3b316dd
         </div>
       </li>
       <li>
         <h3 <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t3']->id); ?>"><?php echo htmlspecialchars($cf['t3']->value); ?></h3>
         <div <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t3_p']->id); ?>"><?php echo htmlspecialchars($cf['t3_p']->value); ?></div>
         <div class="flex-center">
-<<<<<<< HEAD
-          <img height="350px" src="assets/media/Mentor3.png" alt="">
-=======
-          <img height="600" src="assets/media/course-image.jpeg" alt="">
->>>>>>> 836e9c93633212d37e6b27057c2e8b33d3b316dd
+          <img height="350" src="assets/media/course-image.jpeg" alt="">
         </div>
       </li>
       <li>
         <h3 <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t4']->id); ?>"><?php echo htmlspecialchars($cf['t4']->value); ?></h3>
         <div <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t4_p']->id); ?>"><?php echo htmlspecialchars($cf['t4_p']->value); ?></div>
         <div class="flex-center">
-<<<<<<< HEAD
           <img height="380px" src="assets/media/Mentor4.png" alt="">
-=======
-          <img height="600" src="assets/media/course-image.jpeg" alt="">
->>>>>>> 836e9c93633212d37e6b27057c2e8b33d3b316dd
         </div>
       </li>
       <li>
         <h3 <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t5']->id); ?>"><?php echo htmlspecialchars($cf['t5']->value); ?></h3>
         <div <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t5_p']->id); ?>"><?php echo htmlspecialchars($cf['t5_p']->value); ?></div>
         <div class="flex-center">
-<<<<<<< HEAD
           <img height="380px" src="assets/media/Mentor5.png" alt="">
-=======
-          <img height="600" src="assets/media/course-image.jpeg" alt="">
->>>>>>> 836e9c93633212d37e6b27057c2e8b33d3b316dd
         </div>
       </li>
       <li>
         <h3 <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t6']->id); ?>"><?php echo htmlspecialchars($cf['t6']->value); ?></h3>
         <div <?php echo htmlspecialchars($editable); ?> data-id="<?php echo htmlspecialchars($cf['t6_p']->id); ?>"><?php echo htmlspecialchars($cf['t6_p']->value); ?></div>
         <div class="flex-center">
-<<<<<<< HEAD
           <img height="350px" src="assets/media/Mentor6.png" alt="">
-=======
-          <img height="600" src="assets/media/course-image.jpeg" alt="">
->>>>>>> 836e9c93633212d37e6b27057c2e8b33d3b316dd
         </div>
       </li>
     </ul>
